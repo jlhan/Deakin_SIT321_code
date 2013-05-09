32ec39673377b3cbb34ed1cd3d2f73bd8bf6f483
Damon Swayn <damon.swayn@gmail.com>
2013-05-10 09:59:04 +1000

    initial commit
    uploading current work

---
+++ dist_sys_ass_2/default.aspx
@@ -0,0 +1,126 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="dist_sys_ass_2._default" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Distributed Systems Assignment 2</title>
+    <style type="text/css">
+        body
+        {
+            background-color: Lime;
+            font-family: Comic Sans MS;
+        }
+        
+        table
+        {
+            margin-left: auto;
+            margin-right: auto;
+            background-color: Yellow;
+            width: 85%;
+        }
+        
+        h2
+        {
+            text-align: center;
+        }
+        
+        .center_align
+        {
+            text-align: center;
+        }
+    </style>
+
+    <script type="text/javascript">
+        function validate_fname(src, args) {
+            var val = document.getElementById("tb_fname").value;
+            if (val.length < 5 || val.length > 32) {
+                args.isValid = false;
+            }
+            if (new RegExp("\d+", g).test(val) == true) {
+                args.isValid = false;
+            }
+            args.isValid = true;
+        }
+    </script>
+</head>
+<body>
+    <table border="1">
+        <tr>
+            <td colspan="2">
+                <h2>Distributed Systems Assignment 2</h2>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <h3>Instructions:</h3>
+
+                <p>Add: fill in all fields (ID is not necessary) and click on Add Member.</p>
+
+                <p>Search: Fill in all fields relevant to your search (cannot search by database ID) and click on
+                Search. To bring up a members data in the editing window click on the select button in the grid.</p>
+
+                <p>Delete: First, search for the member you wish to delete, select them by hitting the select button to
+                bring their information to the editing window, then click on delete. The grid will then display no results,
+                this means that it has attempted to search for the deleted member and failed, confirming the deletion.</p>
+
+                <p>Modify: First, search for the member you wish to modify, select them by hitting the select button to
+                bring their information to the editing window. Modify the relevant data and then click on Modify. The
+                grid will then display only the modified member with their new updated details, confirming the modification.</p>
+
+                <form id="stuff_form" class="center_align" runat="server">
+                    <asp:Label ID="lbl_id" runat="server">ID: </asp:Label>
+                    <br />
+                    <asp:TextBox ID="tb_ID" runat="server" Enabled="false"></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:Label ID="lbl_fname" runat="server">First Name: </asp:Label>
+                    <br />
+                    <asp:TextBox ID="tb_fname" runat="server"></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:Label ID="lbl_lname" runat="server">Last Name: </asp:Label>
+                    <br />
+                    <asp:TextBox ID="tb_lname" runat="server"></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:Label ID="lbl_memtype" runat="server">Member Type: </asp:Label>
+                    <br />
+                    <asp:TextBox ID="tb_memtype" runat="server"></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:Label ID="lbl_verification" runat="server">Verification Code: </asp:Label>
+                    <br />
+                    <asp:TextBox ID="tb_vcode" runat="server"></asp:TextBox>
+                    <br />
+                    <br />
+                    <asp:Button ID="btn_clear" runat="server" Text="Clear" OnClick="Clear" />
+                    <br />
+                    <br />
+                    <asp:Button ID="btn_search" runat="server" Text="Search Member" OnClick="Search" />
+                    &nbsp;
+                    <asp:Button ID="btn_add" runat="server" Text="Add Member" OnClick="Add" />
+                    &nbsp;
+                    <asp:Button ID="btn_delete" runat="server" Text="Delete Member" OnClick="Delete" />
+                    &nbsp;
+                    <asp:Button ID="btn_modify" runat="server" Text="Modify Member" OnClick="Modify" />
+                    <br />
+                    <br />
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" 
+                        AllowPaging="True" SelectedRowStyle-BackColor="Orange" OnSelectedIndexChanged="Update">
+                        <Columns>
+                            <asp:CommandField ShowSelectButton="True" />
+                            <asp:BoundField DataField="MembershipId" HeaderText="ID" />
+                            <asp:BoundField DataField="FirstName" HeaderText="First Name" />
+                            <asp:BoundField DataField="LastName" HeaderText="Last Name" />
+                            <asp:BoundField DataField="MembershipClass" HeaderText="Membership Class" />
+                            <asp:BoundField DataField="VerificationCode" HeaderText="Verification Code" />
+                        </Columns>
+                    </asp:GridView>
+                </form>
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
---
+++ dist_sys_ass_2/default.aspx.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using MySql;
+
+namespace dist_sys_ass_2
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        public dstempEntities1 entities;
+        public dataEntities1 members;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.entities = new dstempEntities1();
+                this.members = new dataEntities1();
+            }
+            catch
+            {
+                this.entities = null;
+                this.members = null;
+            }
+
+            ////////////////////////////
+            // _INIT_LOAD_MEMBERS(); //
+            ///////////////////////////
+            ///////////////////////////
+            /* for the love of jesus don't run this again! */
+        }
+
+
+        /// <summary>
+        /// use this function and die a horribly slow and painful death.
+        /// </summary>
+        protected void _INIT_LOAD_MEMBERS()
+        {
+            //regular members
+            var employees = from emp in this.entities.employees
+                            where
+                                ((DateTime.Now.Year - emp.birth_date.Year) <= 67 && (DateTime.Now.Year - emp.birth_date.Year) <= 60)
+                            select emp;
+            foreach (employee emp in employees)
+            {
+                this.members.memberships.AddObject(new membership
+                {
+                    FirstName = emp.first_name,
+                    LastName = emp.last_name,
+                    MembershipClass = "regular",
+                    VerificationCode = (emp.birth_date.GetHashCode() + emp.salaries.GetHashCode()).ToString()
+                });
+            }
+            this.members.SaveChanges();
+
+            //get percentages
+            //get salaries
+            var salaries = from s in this.entities.salaries where s.to_date.Year == 9999 select s.salary1;
+            employees = from e in this.entities.employees select e;
+            var TT = 86220;
+            var TF = 69804;
+
+            var ett = (from e in employees where 
+                          (e.salaries.Where(i => i.to_date.Year == 9999).FirstOrDefault().salary1 > TT) 
+                      select e);
+
+            var etf = (from e in employees
+                      where
+                          (e.salaries.Where(i => i.to_date.Year == 9999).FirstOrDefault().salary1 < TT &&
+                          e.salaries.Where(i => i.to_date.Year == 9999).FirstOrDefault().salary1 > TF)
+                      select e);
+
+            //var hash = DateTime.Now.GetHashCode().ToString();
+
+            foreach (employee e in ett)
+            {
+                this.members.memberships.AddObject(new membership
+                {
+                    FirstName = e.first_name,
+                    LastName = e.last_name,
+                    MembershipClass = "gold",
+                    VerificationCode = (e.salaries.GetHashCode() + e.birth_date.GetHashCode()).ToString()
+                });
+            }
+
+            this.members.SaveChanges();
+
+            foreach (employee e in etf)
+            {
+                this.members.memberships.AddObject(new membership
+                {
+                    FirstName = e.first_name,
+                    LastName = e.last_name,
+                    MembershipClass = "silver",
+                    VerificationCode = (e.salaries.GetHashCode() + e.birth_date.GetHashCode()).ToString()
+                });
+            }
+
+            this.members.SaveChanges();
+        }
+
+        protected void Clear(object sender, EventArgs e)
+        {
+            tb_ID.Text = "";
+            tb_fname.Text = "";
+            tb_lname.Text = "";
+            tb_memtype.Text = "";
+            tb_vcode.Text = "";
+
+            GridView1.DataSource = null;
+            DataBind();
+        }
+
+        protected void Search(object sender, EventArgs e)
+        {
+            var search = from emp in this.members.memberships
+                         where
+                             (emp.FirstName == tb_fname.Text) ||
+                             (emp.LastName == tb_lname.Text) ||
+                             (emp.MembershipClass == tb_memtype.Text) ||
+                             (emp.VerificationCode == tb_vcode.Text)
+                         select emp;
+
+            GridView1.DataSource = search;
+            DataBind();
+        }
+
+        protected void Add(object sender, EventArgs e)
+        {
+            membership m = new membership { FirstName = tb_fname.Text, LastName = tb_lname.Text, 
+                MembershipClass = tb_memtype.Text, VerificationCode = tb_vcode.Text };
+            members.memberships.AddObject(m);
+            members.SaveChanges();
+
+            //can't reuse Search() method, so lets just dump the important stuff here
+            var search = from emp in this.members.memberships
+                         where
+                             (emp.FirstName == tb_fname.Text) ||
+                             (emp.LastName == tb_lname.Text) ||
+                             (emp.MembershipClass == tb_memtype.Text) ||
+                             (emp.VerificationCode == tb_vcode.Text)
+                         select emp;
+
+            GridView1.DataSource = search;
+            DataBind();
+        }
+
+        protected void Delete(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(tb_ID.Text);
+            membership m = members.memberships.First(i => i.MembershipId == id);
+            members.memberships.DeleteObject(m);
+            members.SaveChanges();
+
+            //attempt to search for the ID of the deleted member, should return nothing in GridView
+            var ds = from mem in members.memberships where mem.MembershipId == id select mem;
+            GridView1.DataSource = ds;
+            DataBind();
+
+            //ripped out of Clear() method, just cleans up the textboxes.
+            tb_ID.Text = "";
+            tb_fname.Text = "";
+            tb_lname.Text = "";
+            tb_memtype.Text = "";
+            tb_vcode.Text = "";
+        }
+
+        protected void Modify(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(tb_ID.Text); //this line is needed to bypass ado.net restrictions
+            membership m = members.memberships.First(i => i.MembershipId == id); //fetch the member row
+            //do our update
+            m.FirstName = tb_fname.Text;
+            m.LastName = tb_lname.Text;
+            m.MembershipClass = tb_memtype.Text;
+            m.VerificationCode = tb_vcode.Text;
+            members.SaveChanges(); //make changes atomic
+
+            //update gridview to show only updated member
+            var ds = from mem in members.memberships where mem.MembershipId == id select mem;
+            GridView1.DataSource = ds;
+            DataBind();
+        }
+
+        protected void Update(object sender, EventArgs e)
+        {
+            tb_ID.Text = GridView1.SelectedRow.Cells[1].Text;
+            tb_fname.Text = GridView1.SelectedRow.Cells[2].Text;
+            tb_lname.Text = GridView1.SelectedRow.Cells[3].Text;
+            tb_memtype.Text = GridView1.SelectedRow.Cells[4].Text;
+            tb_vcode.Text = GridView1.SelectedRow.Cells[5].Text;
+        }
+    }
+}
